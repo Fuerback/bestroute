4cb6778c6f42ef598cb07efa694325ef25ba9c79
felipe <felipefuerback@hotmail.com>
2021-01-07 07:59:22 -0300

    adicionando teste para inserir rota no controller

--- src/test/java/com/bexstech/exam/controller/RouteControllerTest.java
+++ src/test/java/com/bexstech/exam/controller/RouteControllerTest.java
@@ -4,20 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.io.IOException;
 import java.net.URI;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.bexstech.exam.dto.RouteDTO;
 import com.bexstech.exam.dto.RouteResponseDTO;
 import com.bexstech.exam.service.RouteService;
 
@@ -42,9 +45,22 @@ public class RouteControllerTest {
 				.toUri();
 
 		doReturn( new RouteResponseDTO("GRU-CDG", 20) ).when( routeService ).findRoute( anyString(), any() );
+
 		String forObject = restTemplate.getForObject( targetUrl, String.class );
 
 		assertThat(forObject).contains("GRU-CDG > $20");
 		verify( routeService ).findRoute( anyString(), any() );
+	}
+
+	@Test
+	public void shouldInsertRoute() throws IOException {
+		RouteDTO routeDTO = new RouteDTO( "GRU", "FLN", 25 );
+		HttpEntity<RouteDTO> request = new HttpEntity<>( routeDTO );
+
+		ResponseEntity<Void> response = restTemplate
+				.exchange( "http://localhost:" + port + "/route", HttpMethod.PUT,
+						request, Void.class );
+
+		verify( routeService ).insertRoute( any() );
 	}
 }
